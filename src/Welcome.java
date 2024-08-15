a17c21d2a3a38a4265783a667e504412a0d06310
KHDl <git@github.com:leevquang/FirstPrj.gitt>
2024-08-16 00:53:44 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello LeVanQuang");
+        System.out.println("Welcome to the DHKHDL16A");
+
+    }
+}
